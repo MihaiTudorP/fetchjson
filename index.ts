0349ee956ef8b35417990baf235b93ddbe7681e9
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-10 23:25:12 +0200

    Added interface

--- index.ts
+++ index.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 
 const sourceUrl = "http://jsonplaceholder.typicode.com/todos/4";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(sourceUrl).then(response => {
-  const todo = response.data;
+  const todo = response.data as Todo;
   const id = todo.id;
   const title = todo.title;
   const finished = todo.completed;
