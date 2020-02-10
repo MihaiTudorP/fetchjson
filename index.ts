b1b37132f94be69e433314eefa7810b20e4a5f8c
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-10 23:30:55 +0200

    Extracted arrow function for display

--- index.ts
+++ index.ts
@@ -12,11 +12,15 @@ axios.get(sourceUrl).then(response => {
   const todo = response.data as Todo;
   const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
         The Todo with ID: ${id}
         Has the title: ${title}
-        Is it finished? ${finished ? "yes" : "no"}
+        Is it finished? ${completed ? "yes" : "no"}
     `);
-});
+};
